9b1115db34bfe1221040d2aded94cf53bba754f7
Tareq Assi <42150220+ZF7724@users.noreply.github.com>
2020-05-22 18:31:37 -0400

    Add files via upload

---
+++ Image to Array Converter and Array to Image Converter.py
@@ -0,0 +1,91 @@
+import cv2
+import numpy
+import random
+
+
+
+def imtoArrGrayScaleConverter(image):
+    ArrayG=cv2.imread(image,0)
+    return ArrayG
+
+
+def imtoArrColorConverter(image):
+    ArrayC=cv2.imread(image,1)
+    return ArrayC
+
+def ArrtoImConverter(Array):
+    newImageName=input("Enter new image name with extension: ")
+    Image=cv2.imwrite(newImageName,Array)
+    return Image
+
+
+def ArrayGenerator():
+    List1=[]
+    
+    for i in range(0,64):
+        List1.append(random.randint(0,255))
+        
+        
+    Array=numpy.asarray(List1)
+    FinalArray=Array.reshape(4,4,4)
+        
+    return FinalArray
+    
+
+def menu():
+    
+    print("MAIN MENU")
+    print("==========")
+    
+    print("1. Convert Image to array (Grayscale) ")
+    print("2. Convert Image to array (RGB)")
+    print("3. Computer Generated Image (Small Dimensions in V.01 of this program)")
+    
+        
+    
+    
+def main():
+    
+    menu()
+    
+    userInput=int(input("Enter One of the Above (1-3): "))
+    
+    if userInput==1:
+        ImageName=input("Enter Image File Title With Extension: ")
+        GrayImage=imtoArrGrayScaleConverter(ImageName)
+        print(GrayImage)
+    
+        
+    elif userInput==2:
+        ImageName=input("Enter Image File Title With Extension: ")
+        ColorImage=imtoArrColorConverter(ImageName)
+        print(ColorImage)
+        
+    elif userInput==3:
+        Array=ArrayGenerator()
+        Image=ArrtoImConverter(Array)
+        print(Image)
+    
+
+
+main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
